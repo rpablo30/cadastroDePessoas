90008aa1eb7d7bbee9e34c90b35044fcd648da65
RodrigoPablo <rpablo30@hotmail.com>
2023-04-25 16:33:29 -0300

    Commit com o delete , falando fazer a função

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,18 +1,18 @@
-import { Product } from './../product-model';
+import { Product } from '../product-model';
 
 import { ActivatedRoute, Router } from '@angular/router';
-import { ProductService } from './../product.service';
+import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
   
-  
+ 
 
   constructor(
     private productService :ProductService ,
